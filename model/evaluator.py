e38cf28696d4861f7b90bfccba25d2a65e7fb290
jchaverot <jeremy.chaverot@epfl.ch>
2024-06-02 16:40:16 +0200

    print to remove

--- model/evaluator.py
+++ model/evaluator.py
@@ -464,5 +464,6 @@ if __name__ == '__main__':
         logger.info("Evaluation Completed! Results:")
         logger.info(metrics)
 
+    print(metrics)
     # Write the metrics to a JSON file
     write_json(metrics, "metrics.json")
