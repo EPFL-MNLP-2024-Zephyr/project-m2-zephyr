30007f319233b852649ba19d9677dc04e731663b
jchaverot <jeremy.chaverot@epfl.ch>
2024-06-02 21:41:42 +0200

    add training loop

--- model/train.py
+++ model/train.py
@@ -1,50 +1,80 @@
 import torch
-import json
-from tqdm import tqdm
-import time
-import torch.nn.functional as F
-from torch.utils.data import Dataset
+from trl import DPOConfig, DPOTrainer
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from models.model_dpo import AutoDPOModelForCausalLM
+import torch.nn as nn
+from datasets import Dataset
 
+# Download the pre-trained model and tokenizer from the Hub
+model_name = "Qwen/Qwen1.5-0.5B-Chat"
+model = AutoModelForCausalLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-class DPO_Dataset(Dataset):
-    def __init__(self, file_path):
-        self.data = []
-        with open(file_path, 'r') as f:
-            for line in f:
-                self.data.append(json.loads(line))
+# Initialize your model class and import the pre-trained model into your class
+# Note that if you have a custom module in your class
+# You should initialize the weights of this module in the `__init__` function
+model_wrapper = AutoDPOModelForCausalLM(pretrained_model=model)
 
-    def __len__(self):
-        return len(self.data)
+# Define the directory where you want to save the model and tokenizer
+save_directory = "/checkpoint/"
 
-    def __getitem__(self, idx):
-        return self.data[idx]
+# Save the model and tokenizer
+# model_wrapper.save_pretrained(save_directory)
+# tokenizer.save_pretrained(save_directory)
 
+# We used several datasets for training, this one is an example of train dataset for the format
+# You need to load here your own dataset
+dpo_dataset_dict = {
+    "prompt": [
+        "hello",
+        "how are you",
+        "What is your name?",
+        "What is your name?",
+        "Which is the best programming language?",
+        "Which is the best programming language?",
+        "Which is the best programming language?",
+    ],
+    "chosen": [
+        "hi nice to meet you",
+        "I am fine",
+        "My name is Mary",
+        "My name is Mary",
+        "Python",
+        "Python",
+        "Java",
+    ],
+    "rejected": [
+        "leave me alone",
+        "I am not fine",
+        "Whats it to you?",
+        "I dont have a name",
+        "Javascript",
+        "C++",
+        "C++",
+    ],
+}
 
-def train(model, dataloader, optimizer, reference_model, tokenizer, num_epochs=3):
-    model.train()
-    for epoch in tqdm(range(num_epochs), desc="Epochs"):
-        total_loss = 0
-        start_time = time.time()
-        for batch in tqdm(dataloader, desc="Batches", leave=False):
-            optimizer.zero_grad()
+# Convert the dictionary to a datasets.Dataset
+train_dataset = Dataset.from_dict(dpo_dataset_dict)
 
-            chosen_logps, rejected_logps = model.get_logprobs(batch, tokenizer)
-            reference_chosen_logps, reference_rejected_logps = reference_model.get_logprobs(batch, tokenizer)
+# Configure the training arguments for DPO
+training_args = DPOConfig(
+    beta=0.1,
+)
 
-            rewards = model.prediction_step_reward(chosen_logps,
-                                                   rejected_logps,
-                                                   reference_chosen_logps,
-                                                   reference_rejected_logps)
+# Configure the DPO trainer
+dpo_trainer = DPOTrainer(
+    model_wrapper,
+    args=training_args,
+    beta=0.1,
+    train_dataset=train_dataset,
+    tokenizer=tokenizer,
+    max_prompt_length=512,
+    max_length=1024,
+)
 
-            chosen_rewards = rewards["chosen_rewards"]
-            rejected_rewards = rewards["rejected_rewards"]
+# Train the model with DPO
+dpo_trainer.train()
 
-            dpo_loss = - F.logsigmoid(chosen_rewards - rejected_rewards)
-            dpo_loss.backward()
-            optimizer.step()
-
-            total_loss += dpo_loss.item()
-
-        # Calculate time taken for epoch
-        epoch_time = time.time() - start_time
-        tqdm.write(f"Epoch {epoch+1}/{num_epochs} - Avg Loss: {total_loss / len(dataloader):.4f} - Time: {epoch_time:.2f} s")
+# Save the trained model
+dpo_trainer.model.save_pretrained(save_directory)
